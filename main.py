92a716b8cf3d3c478b5a4de5ba0602ce09c86912
sathyakrishna-sharma <68376764+sathyakrishna-sharma@users.noreply.github.com>
2023-05-14 12:58:57 +0530

    Add files via upload
    Added the student_class

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import openai
-from science_cross_questioning_functions import get_primary_answer, get_secondary_answer,create_prompts
+import os
+from science_cross_questioning_functions import get_primary_answer, get_secondary_answer,create_prompts, mcq_cross_questioning
 
+openai.api_key = ''
 
-
-input_query = 'what is rainbow'
-primary_answer, primary, secondary = get_primary_answer(input_query)
-secondary_answer = get_secondary_answer(input_query)
+input_query = 'what is a rainbow?'
+student_class = '10'
+primary_answer, primary, secondary = get_primary_answer(input_query, student_class)
+secondary_answer = get_secondary_answer(input_query, student_class)
 prompts_output = create_prompts(primary, secondary)
+mcq_science = mcq_cross_questioning(prompts_output, student_class)
 
 print(input_query)
 print(primary_answer)
 print(primary,secondary)
 print(secondary_answer)
 print(prompts_output)
+print(mcq_science)
