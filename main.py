ab30fa46fefe64d4d119d8c25de0413a965813dd
sathyakrishna-sharma <68376764+sathyakrishna-sharma@users.noreply.github.com>
2023-05-16 16:31:31 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import openai
 import os
 from science_cross_questioning_functions import get_primary_answer, get_secondary_answer,create_prompts, mcq_cross_questioning
 
-openai.api_key = ''
+openai.api_key = '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'
 
-input_query = 'what is a rainbow?'
+input_query = 'what is a radiowave?'
 student_class = '10'
 primary_answer, primary, secondary = get_primary_answer(input_query, student_class)
-secondary_answer = get_secondary_answer(input_query, student_class)
 prompts_output = create_prompts(primary, secondary)
+input_prompt_question = prompts_output[0]
+secondary_answer = get_secondary_answer(input_prompt_question, student_class)
 mcq_science = mcq_cross_questioning(prompts_output, student_class)
 
 print(input_query)
 print(primary_answer)
 print(primary,secondary)
-print(secondary_answer)
 print(prompts_output)
+print(secondary_answer)
 print(mcq_science)
