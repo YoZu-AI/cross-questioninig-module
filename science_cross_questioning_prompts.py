de24a42abfed3de2e53ef9e162e33dba557297d1
sathyakrishna-sharma <68376764+sathyakrishna-sharma@users.noreply.github.com>
2023-05-19 15:35:20 +0530

    Add files via upload
    Making the create_prompt function prompt diverse

--- science_cross_questioning_prompts.py
+++ science_cross_questioning_prompts.py
@@ -9,7 +9,7 @@ primary_concepts:
 """ 
 
 prompt_get_questions = """
-Generate 2 concise questions connecting any one random entity from the list {list_secondary_concepts} to the {list_primary_concepts} for each question    
+Generate 2 concise questions. For each question connect any one random entity from the list {list_secondary_concepts} to the {list_primary_concepts}.  
 """
 
 prompt_MCQ = """ 
